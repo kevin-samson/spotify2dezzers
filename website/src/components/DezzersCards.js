74299280767f23503ed4922519050270018efafd
Kevin Samson <kevinsam2003@gmail.com>
2021-05-06 19:06:57 +0400

    Added progress bar

--- website/src/components/DezzersCards.js
+++ website/src/components/DezzersCards.js
@@ -1,22 +1,25 @@
-import React from 'react'
+import React from "react";
+import { ProgressBar } from "react-bootstrap";
 
-export default function DezzersCards({name,creator,image}) {
-    return (
+export default function DezzersCards({ name, creator, image }) {
+	return (
 		<div
-			className="d-flex align-items-center"
+			className="d-flex"
 			style={{
 				border: "2px solid #dedede",
 				borderRadius: "5px",
 				padding: "10px",
 				backgroundColor: "#f1f1f1",
 				minWidth: "20rem",
-				
 			}}
 		>
-			<img src={image} style={{ height: "64px" }} alt="somthinf"></img>
+			<img src={image} style={{ height: "64px" }} alt="missing"></img>
 			<div className="ml-2">
 				<div>{name}</div>
 				<div className="text-muted">{creator}</div>
+			</div>
+			<div className="mx-auto ml-2 mt-4" style={{ minWidth: "20rem" }}>
+				<ProgressBar variant="success" now={40} />
 			</div>
 		</div>
 	);
